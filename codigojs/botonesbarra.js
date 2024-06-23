0e2799020557e381a781d13b08d9eb377b836b25
lugengar <luciano.barbinii@gmail.com>
2024-06-23 18:28:16 -0300

    a

--- codigojs/botonesbarra.js
+++ codigojs/botonesbarra.js
@@ -3,4 +3,6 @@ const barra = document.querySelectorAll('.barradebusqueda');
 function barradebusqueda(id){
     barra.forEach(barra => barra.classList.remove('activo'));
     document.getElementById(id).classList.add('activo');
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+
 }
