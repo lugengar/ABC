95f01f1c8ded5cddcd8517a6d6aba45e7e313bf0
lugengar <luciano.barbinii@gmail.com>
2024-08-31 03:34:40 -0300

    a

--- abc.sql
+++ abc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-08-2024 a las 18:35:31
+-- Tiempo de generación: 28-08-2024 a las 21:40:33
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
